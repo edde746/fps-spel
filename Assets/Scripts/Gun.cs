c61cf2605c89efa77d1a9bb1fcf820870545107d
edde746 <86283021+edde746@users.noreply.github.com>
2022-02-10 15:32:14 +0100

    Limited Ammo

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using TMPro;
 
 public class Gun : MonoBehaviour
 {
@@ -12,23 +13,56 @@ public class Gun : MonoBehaviour
 
     float cooldown = 0f;
 
+    public int bulletsInMagazine = 20;
+    public int magazineSize = 20;
+    public float reloadTime = 17f;
+    TextMeshProUGUI counter;
+    bool reloading = false;
+
+    void Start()
+    {
+        counter = GameObject.FindGameObjectWithTag("BulletCounter").GetComponent<TextMeshProUGUI>();
+    }
+
     void Update()
     {
         cooldown -= Time.deltaTime;
-        if (Input.GetButton("Fire1") && cooldown <= 0f)
+        if (reloading && cooldown <= 0f)
         {
+            counter.text = $"{magazineSize}/{magazineSize}";
+            reloading = false;
+        }
+
+        if (Input.GetButton("Fire1") && cooldown <= 0f && bulletsInMagazine > 0)
+        {
+            bulletsInMagazine -= 1;
+            counter.text = $"{bulletsInMagazine}/{magazineSize}";
+            if (bulletsInMagazine <= 0)
+            {
+                cooldown = reloadTime;
+                bulletsInMagazine = magazineSize;
+                counter.text = "Reloading...";
+                reloading = true;
+            }
+
             muzzleFlash.Play();
             RaycastHit ray;
             var hit = Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out ray);
-            if (hit) {
+            if (hit)
+            {
                 Instantiate(impactParticle, ray.point, Quaternion.identity);
                 var target = ray.transform.GetComponent<Target>();
-                if (target) {
+                if (target)
+                {
                     target.ApplyDamage(damage);
                 }
             }
 
-            cooldown = 1f/fireRate;
+            cooldown = 1f / fireRate;
         }
+
+        if (counter && cooldown <= 0f)
+            counter.text = $"{bulletsInMagazine}/{magazineSize}";
+
     }
 }
